b4ec44d3276af0224cb6f9d3c1b410eef4f7029c
bd1__git <reichholf.martin@gmail.com>
2019-06-17 15:20:26 +0200

    rough finish of layer

--- tensorflow/layers/layer.nim
+++ tensorflow/layers/layer.nim
@@ -1,5 +1,8 @@
+import sequtils
 import ../ops/ops
 import ../core/core
+import ./optim
+import ./variable
 {.hint[XDeclaredButNotUsed]:off.}
 
 type Layer* = ref object of RootObj
@@ -10,13 +13,24 @@ method `$`*(layer: Layer): string {.base.} = "Layer"
 method make(layer: Layer, root: Scope): (proc(rt: Scope, input: Out): Out) {.base.} = 
     raise newException(ValueError, "Not Implemented. Please overload `make` for your Layer")
 
-proc build*(layers: seq[Layer], root: Scope, input: Out): Out = 
+proc build*(layers: seq[Layer], root: Scope, input: Out, opt: Optim): OutList = 
     var outp = layers[0].make(root)(root, input)
+    var vars: seq[Variable]
 
     for layer in layers[1..^1]:
         outp = layer.make(root)(root, outp)
+        vars = vars.concat(layer.train)
 
-    outp
+    if vars.len != 0:
+        var grads: OutList
+        let varsAsOutList = newOutList(vars.map(proc(vvar: Variable): Out = vvar.vvar))
+        root.addSymbolicGradients(outp, varsAsOutList, grads)
+
+        let outpList = opt.make(root)(root, vars, grads)
+
+        return outpList
+    else:
+        return newOutList(outp)
 
 export Layer,
        `$`,
