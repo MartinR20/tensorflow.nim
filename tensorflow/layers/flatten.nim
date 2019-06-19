3c9f887138f3d86303ae428a880ad68679093dac
bd1__git <reichholf.martin@gmail.com>
2019-06-19 22:15:45 +0200

    started flatten

---
+++ tensorflow/layers/flatten.nim
@@ -0,0 +1,23 @@
+import options
+import ../ops/ops
+import ../core/core
+import ./layer
+{.hint[XDeclaredButNotUsed]:off.}
+
+type Flatten* = ref object of Layer
+
+method `$`*(layer: Flatten): string = "Flatten"
+
+method make(layer: Flatten, root: Scope): proc(rt: Scope, input: Out): Out = 
+    return proc(rt: Scope, input: Out): Out = 
+        #TODO: make calculation right
+        return rt.Reshape(input, rt.Prod(rt.Shape(input), rt.Const(0)))
+
+proc newFlatten(model: var seq[Layer]) =
+    var flatten = new Flatten
+    model.add(flatten)
+
+export Flatten,
+       `$`,
+       newFlatten,
+       make
