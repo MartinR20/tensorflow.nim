683c2d20e873c8362b108f530164425ec277d0fc
bd1__git <reichholf.martin@gmail.com>
2019-07-27 16:30:45 +0200

    removed unnecessary test

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-zero_out_module = tf.load_op_library('./libnewOp_test.so')
-print(zero_out_module.OP_LIST)
-with tf.compat.v1.Session(''):
-    result = zero_out_module.zero_out([5, 4, 3, 2, 1])
-    print(result)
