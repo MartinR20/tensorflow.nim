e92aa02160a9a7dd9ed33914890b06faebf85892
Martin Reichholf <martin@Martins-MBP.home>
2019-08-10 07:38:03 +0200

    removed summary writer

--- tensorflow/layers/layer.nim
+++ tensorflow/layers/layer.nim
@@ -187,9 +187,6 @@ proc compile*[N](layers: seq[Layer], root: Scope, loss: Loss, optim: Optim[N]): 
             let opted = opt(rtNamed, vars, grads)
             let debug = lloss(rtNamed, rtNamed.eval(placeholder_x), placeholder_y)
 
-            let writer = newSummaryWriter("./summary/event")
-            writer.write_grapdef(rt.toGraphDef())
-
             let sess = newSession(rtNamed)
 
             sess.runSessionVoid(initVars)
