3e33b7ed2fafb841386a0b4b6432a63d914be8d6
Martin Reichholf <martin@Martins-MBP.home>
2019-08-10 07:37:53 +0200

    finished ccpgen

--- tensorflow/ops/newop/newop.nim
+++ tensorflow/ops/newop/newop.nim
@@ -2,6 +2,8 @@ import ../../core/core
 import ../../utils/utils
 import macros
 import tables
+import strutils
+import sequtils
 {.hint[XDeclaredButNotUsed]:off.}
 
 type
@@ -77,6 +79,199 @@ macro setShapeFn(fn: proc(ctx: ptr InferenceContext): Status, x: untyped): untyp
   
   nnkStmtList.newTree(fun, x)
 
+proc makeArgsCPP(table: TableRef[string,string]): string =
+  var ret: seq[string]
+
+  for name, dtype in table.pairs:
+    ret.add "       " & dtype & " " & name
+
+  return ret.join(",\n")
+
+proc makeArgsNim(table: TableRef[string,string]): string =
+  var ret: seq[string]
+
+  for name, dtype in table.pairs:
+    ret.add name & ": " & (if dtype == "::tensorflow::Input": "Out" else: dtype)
+
+  return ret.join(", ")
+  
+proc convInputsToOutputs(table: TableRef[string,string]): string =
+  var ret: string
+
+  for name in table.keys:
+    ret &= "       auto _" & name & " = ::tensorflow::ops::AsNodeOut(scope, " & name & ");\n"
+    ret &= "       if (!scope.ok()) return;\n"
+
+  return ret
+
+proc makeBuilderInputs(table: TableRef[string,string]): string =
+  var ret: string
+
+  for name in table.keys:
+    ret &= "                                    .Input(_" & name & ")\n"
+
+  return ret
+  
+proc makeBuilderAttrs(table: TableRef[string,string]): string =
+  var ret: string
+
+  for name in table.keys:
+    ret &= "                                    .Attr(\"" & name & "\", " & name & ")\n"
+
+  return ret
+
+const nimKeywordsTranslate = { 
+  "addr"       : "naddr"      ,           
+  "and"        : "nand"       ,         
+  "as"         : "nas"        ,        
+  "asm"        : "nasm"       ,        
+  "bind"       : "nbind"      ,          
+  "block"      : "nblock"     ,           
+  "break"      : "nbreak"     ,          
+  "case"       : "ncase"      ,          
+  "cast"       : "ncast"      ,          
+  "concept"    : "nconcept"   ,             
+  "const"      : "nconst"     ,           
+  "continue"   : "ncontinue"  ,              
+  "converter"  : "nconverter" ,              
+  "defer"      : "ndefer"     ,           
+  "discard"    : "ndiscard"   ,             
+  "distinct"   : "ndistinct"  ,              
+  "div"        : "ndiv"       ,         
+  "do"         : "ndo"        ,       
+  "elif"       : "nelif"      ,          
+  "else"       : "nelse"      ,          
+  "end"        : "nend"       ,         
+  "enum"       : "nenum"      ,          
+  "except"     : "nexcept"    ,            
+  "export"     : "nexport"    ,           
+  "finally"    : "nfinally"   ,             
+  "for"        : "nfor"       ,         
+  "from"       : "nfrom"      ,          
+  "func"       : "nfunc"      ,         
+  "if"         : "nif"        ,        
+  "import"     : "nimport"    ,            
+  "in"         : "nin"        ,        
+  "include"    : "ninclude"   ,             
+  "interface"  : "ninterface" ,               
+  "is"         : "nis"        ,               
+  "isnot"      : "nisnot"     ,               
+  "iterator"   : "niterator"  ,                
+  "let"        : "nlet"       ,                
+  "macro"      : "nmacro"     ,      
+  "method"     : "nmethod"    ,       
+  "mixin"      : "nmixin"     ,      
+  "mod"        : "nmod"       ,    
+  "nil"        : "nnil"       ,     
+  "not"        : "nnot"       ,        
+  "notin"      : "nnotin"     ,        
+  "object"     : "nobject"    ,            
+  "of"         : "nof"        ,        
+  "or"         : "nor"        ,        
+  "out"        : "nout"       ,        
+  "proc"       : "nproc"      ,          
+  "ptr"        : "nptr"       ,        
+  "raise"      : "nraise"     ,           
+  "ref"        : "nref"       ,         
+  "return"     : "nreturn"    ,           
+  "shl"        : "nshl"       ,         
+  "shr"        : "nshr"       ,         
+  "static"     : "nstatic"    ,           
+  "template"   : "ntemplate"  ,              
+  "try"        : "ntry"       ,         
+  "tuple"      : "ntuple"     ,           
+  "type"       : "ntype"      ,         
+  "using"      : "nusing"     ,          
+  "var"        : "nvar"       ,        
+  "when"       : "nwhen"      ,          
+  "while"      : "nwhile"     ,          
+  "xor"        : "nxor"       ,        
+  "yield"      : "nyield"              
+}.toTable()
+
+const cppKeywordsTranslate = { 
+  "asm"              : "casm"              ,    
+  "else"             : "celse"             ,     
+  "new"              : "cnew"              ,    
+  "this"             : "cthis"             ,     
+  "auto"             : "cauto"             ,     
+  "enum"             : "cenum"             ,     
+  "operator"         : "coperator"         ,         
+  "throw"            : "cthrow"            ,      
+  "bool"             : "cbool"             ,     
+  "explicit"         : "cexplicit"         ,         
+  "private"          : "cprivate"          ,        
+  "true"             : "ctrue"             ,     
+  "break"            : "cbreak"            ,      
+  "export"           : "cexport"           ,       
+  "protected"        : "cprotected"        ,          
+  "try"              : "ctry"              ,    
+  "case"             : "ccase"             ,       
+  "extern"           : "cextern"           ,       
+  "public"           : "cpublic"           ,       
+  "typedef"          : "ctypedef"          ,        
+  "catch"            : "ccatch"            ,      
+  "false"            : "cfalse"            ,      
+  "register"         : "cregister"         ,         
+  "typeid"           : "ctypeid"           ,       
+  "char"             : "cchar"             ,     
+  "float"            : "cfloat"            ,      
+  "reinterpret_cast" : "creinterpret_cast" ,                 
+  "typename"         : "ctypename"         ,         
+  "class"            : "cclass"            ,      
+  "for"              : "cfor"              ,    
+  "return"           : "creturn"           ,       
+  "union"            : "cunion"            ,      
+  "const"            : "cconst"            ,      
+  "friend"           : "cfriend"           ,       
+  "short"            : "cshort"            ,                        
+  "unsigned"         : "cunsigned"         ,         
+  "const_cast"       : "cconst_cast"       ,           
+  "goto"             : "cgoto"             ,     
+  "signed"           : "csigned"           ,       
+  "using"            : "cusing"            ,      
+  "continue"         : "ccontinue"         ,         
+  "if"               : "cif"               ,   
+  "sizeof"           : "csizeof"           ,       
+  "virtual"          : "cvirtual"          ,        
+  "default"          : "cdefault"          ,        
+  "inline"           : "cinline"           ,       
+  "static"           : "cstatic"           ,       
+  "void"             : "cvoid"             ,     
+  "delete"           : "cdelete"           ,       
+  "int"              : "cint"              ,    
+  "static_cast"      : "cstatic_cast"      ,            
+  "volatile"         : "cvolatile"         ,         
+  "do"               : "cdo"               ,   
+  "long"             : "clong"             ,     
+  "struct"           : "cstruct"           ,       
+  "wchar_t"          : "cwchar_t"          ,        
+  "double"           : "cdouble"           ,       
+  "mutable"          : "cmutable"          ,        
+  "switch"           : "cswitch"           ,       
+  "while"            : "cwhile"            ,      
+  "dynamic_cast"     : "cdynamic_cast"     ,             
+  "namespace"        : "cnamespace"        ,          
+  "template"         : "ctemplate"         ,                           
+  "And"              : "cAnd"              ,    
+  "bitor"            : "cbitor"            ,      
+  "not_eq"           : "cnot_eq"           ,       
+  "xor"              : "cxor"              ,                      
+  "and_eq"           : "cand_eq"           ,       
+  "compl"            : "ccompl"            ,      
+  "or"               : "cor"               ,   
+  "xor_eq"           : "cxor_eq"           ,       
+  "bitand"           : "cbitand"           ,       
+  "not"              : "cnot"              ,    
+  "or_eq"            : "cor_eq"                  
+}.toTable()
+
+proc getOrReturn[M, N](table: Table[M, N], key: M): N =
+  if table.hasKey(key):
+    return table[key]
+  else:
+    return key
+
 macro tfexport(device: string, x: untyped): untyped =
   var exportName = $name(x)
 
@@ -86,18 +281,98 @@ macro tfexport(device: string, x: untyped): untyped =
   else:
     ddevice = $device
 
-  var cppSource =  "\"\"class " & exportName & "Op : public tensorflow::OpKernel {\n  public: \n    explicit " & exportName & "Op(tensorflow::OpKernelConstruction* context): tensorflow::OpKernel(context) {}\n    void Compute(tensorflow::OpKernelContext* context) override;\n    }; \nREGISTER_KERNEL_BUILDER(Name(\"" & exportName & "\").Device(\"" & ddevice & "\"), " & exportName & "Op);\"\""
+  var ins = newTable[string, string]()
+  var attrs = newTable[string, string]()
+  var outputIsList = false
+
+  for pragma in lookUp[$name(x)].split("."):
+    let splited = pragma
+                    .replace("(", " ")
+                    .replace(")", " ")
+                    .replace("\"", "")
+                    .replace(":", "")
+                    .split(" ")
+
+    let name = cppKeywordsTranslate.getOrReturn(
+                nimKeywordsTranslate.getOrReturn(splited[1])
+               )
+
+    if splited[0] == "Input":
+      ins[name] = "::tensorflow::Input"
+
+    elif splited[0] == "Attr" and splited[2] != "type":
+      attrs[name] = splited[2]
+
+    elif splited[0] == "Output" and splited.contains("*"):
+      outputIsList = true
+
+  var cppSource =  "\"\"class " & exportName & "Op : public tensorflow::OpKernel {\n" &
+                   "  public: \n" &
+                   "    explicit " & exportName & "Op(tensorflow::OpKernelConstruction* context): tensorflow::OpKernel(context) {}\n" &
+                   "    void Compute(tensorflow::OpKernelContext* context) override { " & exportName & "_Compute(context); }\n" &
+                   "}; \n" &
+                   "REGISTER_KERNEL_BUILDER(Name(\"" & exportName & "\").Device(\"" & ddevice & "\"), " & exportName & "Op);\n" &
+                   "class " & exportName & "{\n" &
+                   "  public:\n" &
+                   "    " & exportName & "(const ::tensorflow::Scope& scope,\n" & 
+                   makeArgsCPP(ins) & ",\n" &
+                   makeArgsCPP(attrs) & ") {\n" &
+                   "       if (!scope.ok()) return;\n" &
+                   convInputsToOutputs(ins) &
+                   "       ::tensorflow::Node* ret;\n" &
+                   "       const auto unique_name = scope.GetUniqueNameForOp(\"" & exportName & "\");\n" &
+                   "       auto builder = ::tensorflow::NodeBuilder(unique_name, \"" & exportName & "\")\n" &
+                   makeBuilderInputs(ins) &
+                   makeBuilderAttrs(attrs) &
+                   "       ;\n" &
+                   "       scope.UpdateBuilder(&builder);\n" &
+                   "       scope.UpdateStatus(builder.Finalize(scope.graph(), &ret));\n" &
+                   "       if (!scope.ok()) return;\n" &
+                   "       scope.UpdateStatus(scope.DoShapeInference(ret));\n" &
+                   "       this->operation = ::tensorflow::Operation(ret);\n" 
+  
+  if outputIsList:
+    cppSource &= "       for (int32_t i = 0; i < ret->num_outputs(); ++i)\n" &
+                 "         this->output.push_back(::tensorflow::Output(ret, i));\n" &
+                 "    }\n" & 
+                 "\n" &
+                 "    ::tensorflow::Output operator[](size_t index) const { return output[index]; }\n" &
+                 "\n" &
+                 "    ::tensorflow::Operation operation;\n" &
+                 "    ::tensorflow::OutputList output;\n"
+  else:
+    cppSource &= "       this->output = ::tensorflow::Output(ret, i);\n" &
+                 "    }\n" & 
+                 "\n" &
+                 "    ::tensorflow::Operation operation;\n" &
+                 "    ::tensorflow::Output output;\n"
+
+  cppSource &= "};\n\"\""
 
   var funbody = body(x) 
-  var funheader = parseStmt("proc export" & exportName & "(ctx: ptr OpKernelContext) {.exportc:\"" & exportName & "Op::Compute\", asmNoStackFrame, noconv.}")
+  var funheader = parseStmt("proc export" & exportName & "(ctx: ptr OpKernelContext) {.exportc:\"" & exportName & "_Compute\".}")
 
   funheader[0].del(funheader[0].len-1, 1)
   insert(funheader[0], funheader[0].len, funbody)
-  insert(funheader, 0, parseStmt("{.emit:\"" & cppSource & "\".}"))
-  insert(funheader, 0, parseStmt("{.emit:lookUp[\"" & $name(x) & "\"] & \";\".}"))
+
+  let nimSource = "proc " & exportName & "(root: Scope, " &  
+                    makeArgsNim(ins) & ", " & 
+                    makeArgsNim(attrs) & "): " & 
+                    (if outputIsList: "OutList" else: "Out") & 
+                    " {.importcpp:\"" & exportName & "(*#" & ", #".repeat(ins.len + attrs.len) & ").output\".}"
+
+  insert(funheader, 1, parseStmt(nimSource))
+  insert(funheader, 1, parseStmt("{.emit:\"" & cppSource & "\".}"))
+  insert(funheader, 1, parseStmt("{.emit:lookUp[\"" & $name(x) & "\"] & \";\".}"))
 
   if not op_included:
-    var includes = "\"\"#include \"tensorflow/core/framework/op.h\" \n#include \"tensorflow/core/framework/shape_inference.h\" \n#include \"tensorflow/core/framework/op_kernel.h\" \n\"\""
+    var includes = "\"\"#include \"tensorflow/core/framework/op.h\" \n" & 
+                   "#include \"tensorflow/core/framework/shape_inference.h\" \n" & 
+                   "#include \"tensorflow/core/framework/op_kernel.h\" \n" & 
+                   "#include \"tensorflow/cc/framework/scope.h\" \n" & 
+                   "#include \"tensorflow/core/framework/tensor.h\" \n" & 
+                   "#include \"tensorflow/core/framework/tensor_shape.h\" \n" & 
+                   "#include \"tensorflow/core/framework/types.h\" \n\"\""
     insert(funheader, 0, parseStmt("{.emit:\"" & includes & "\".}"))
     op_included = true
 
