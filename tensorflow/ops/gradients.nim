6dfcc8d787170f593acac5ff54b4ef2291ae7a8b
Martin Reichholf <martin@Martins-MBP.home>
2019-08-11 15:09:08 +0200

    removed emit

--- tensorflow/ops/gradients.nim
+++ tensorflow/ops/gradients.nim
@@ -5,53 +5,7 @@ import ../core/core,
        ./custom_ops,
        sequtils
 
-{.emit:"""
-#include "tensorflow/core/framework/op.h"  
-#include "tensorflow/core/framework/shape_inference.h"
-#include "tensorflow/core/framework/op_kernel.h"
-#include "tensorflow/cc/framework/scope.h"
-#include "tensorflow/core/framework/tensor.h"
-#include "tensorflow/core/framework/tensor_shape.h"
-#include "tensorflow/core/framework/types.h"
-
-using namespace tensorflow;
-
-class ConcatOffset {
- public:
-  ConcatOffset(const ::tensorflow::Scope& scope, ::tensorflow::Input concat_dim,
-             ::tensorflow::InputList shape);
-  ::tensorflow::Output operator[](size_t index) const { return offset[index]; }
-
-
-  Operation operation;
-  ::tensorflow::OutputList offset;
-};
-
-ConcatOffset::ConcatOffset(const ::tensorflow::Scope& scope,
-                           ::tensorflow::Input concat_dim,
-                           ::tensorflow::InputList shape) {
-  if (!scope.ok()) return;
-  auto _concat_dim = ::tensorflow::ops::AsNodeOut(scope, concat_dim);
-  if (!scope.ok()) return;
-  auto _shape = ::tensorflow::ops::AsNodeOutList(scope, shape);
-  if (!scope.ok()) return;
-  ::tensorflow::Node* ret;
-  const auto unique_name = scope.GetUniqueNameForOp("ConcatOffset");
-  auto builder = ::tensorflow::NodeBuilder(unique_name, "ConcatOffset")
-                     .Input(_concat_dim)
-                     .Input(_shape)
-  ;
-  scope.UpdateBuilder(&builder);
-  scope.UpdateStatus(builder.Finalize(scope.graph(), &ret));
-  if (!scope.ok()) return;
-  scope.UpdateStatus(scope.DoShapeInference(ret));
-  this->operation = Operation(ret);
-  for (int32 i = 0; i < ret->num_outputs(); ++i)
-    this->offset.push_back(Output(ret, i));
-}
-""".}
-
-proc ConcatOffset(scope: Scope, concat_dim: Out, shape: OutList): OutList {.importcpp:"ConcatOffset(*#, #, #).offset".}
+makeExsistingOp("ConcatOffset", "(scope: Scope, concat_dim: Out, shape: OutList): OutList")
 #proc Stack(scope: Scope, list: OutList, axis: Out): Out {.importcpp:"tensorflow::ops::Stack(*#, #, #)".}
 
 proc ConcatV2(scope: Scope, op: Operation, gradInputs: OutList, gradOutputs: ptr OutList): Status {.grad.} =
