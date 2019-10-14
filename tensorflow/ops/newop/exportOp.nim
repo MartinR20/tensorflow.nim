abd56d6875fde0475489b1f5211d8f6e8f2a2bc2
Martin Reichholf <martin@Martins-MBP.home>
2019-10-14 20:56:19 +0200

    bugfix

--- tensorflow/ops/newop/exportOp.nim
+++ tensorflow/ops/newop/exportOp.nim
@@ -11,40 +11,6 @@ const
   CPU: string = "CPU"
   GPU: string = "GPU"
 
-proc getInsAndAttrs(exportName: string): (OrderedTable[string, string], OrderedTable[string, string], bool) = 
-  var ins = initOrderedTable[string, string]()
-  var attrs = initOrderedTable[string, string]()
-  var outputIsList = false
-
-  if not lookUp.hasKey(exportName):
-    raise newException(ValueError, "You didn't register any input or output for your function!")
-
-  for pragma in lookUp[exportName].split("."):
-    let splited = pragma
-                    .replace("(", " ")
-                    .replace(")", " ")
-                    .replace("\"", "")
-                    .replace(":", "")
-                    .split(" ")
-
-    let name = cppKeywordsTranslate.getOrReturn(
-                nimKeywordsTranslate.getOrReturn(splited[1])
-              )
-
-    if splited[0] == "Input":
-      if splited.contains("*"):
-        ins[name] = "::tensorflow::InputList"
-      else:
-        ins[name] = "::tensorflow::Input"
-
-    elif splited[0] == "Attr" and splited[2] != "type":
-      attrs[name] = splited[2]
-
-    elif splited[0] == "Output" and splited.contains("*"):
-      outputIsList = true
-
-  return (ins, attrs, outputIsList)
-
 proc makeOpKernel(exportName: string, funheader: NimNode): NimNode =
   let cppSource =  "\"\"class " & exportName & "Op : public tensorflow::OpKernel {\n" &
                     "  public: \n" &
@@ -54,11 +20,6 @@ proc makeOpKernel(exportName: string, funheader: NimNode): NimNode =
 
   insert(funheader, 1, parseStmt("{.emit:\"" & cppSource & "\".}"))
   return funheader
-
-proc makeFun(body: NimNode, header: NimNode): NimNode =
-  header[0].del(header[0].len-1, 1)
-  insert(header[0], header[0].len, body)
-  return header
 
 proc makeExportFun(exportName: string, body: NimNode): NimNode =
   return makeFun(body,
@@ -77,9 +38,10 @@ macro tfexport(device: string, x: untyped): untyped =
   var funheader = makeExportFun(exportName, body(x))
 
   let (ins, attrs, outputIsList) = getInsAndAttrs(exportName)  
+  
 
-  funheader = makeNimDef(exportName, ins, attrs, outputIsList, funheader)
-  funheader = makeOpClass(exportName, ins, attrs, outputIsList, funheader)
+  #funheader = makeNimDef(exportName, ins, attrs, outputIsList, funheader)
+  #funheader = makeOpClass(exportName, ins, attrs, outputIsList, funheader)
   funheader = REGISTER_KERNEL_BUILDER(exportName, ddevice, funheader)
   funheader = makeOpKernel(exportName, funheader)
   funheader = REGISTER_OP(exportName, funheader)
@@ -87,7 +49,8 @@ macro tfexport(device: string, x: untyped): untyped =
 
   return funheader
 
-export tfexport,
+export getInsAndAttrs,
+       tfexport,
        CPU,
        GPU
        
