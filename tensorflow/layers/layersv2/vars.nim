1ffcb1ab82b86236073610eb1b9718233273133f
Martin Reichholf <martin@Martins-MBP.home>
2019-10-14 21:07:57 +0200

    added vars

---
+++ tensorflow/layers/layersv2/vars.nim
@@ -0,0 +1,29 @@
+import 
+    globals, json, macros, tables
+
+proc vars*(prgm: NimNode, model: string, scope: NimNode, i: int, command: NimNode) =
+    let name = $command[1]
+
+    metadata[model].add %*{
+                    "name": name,
+                    "shape": metadata[model][i-1]["shape"],
+                    "dtype": metadata[model][i-1]["dtype"],
+                    "vars": []
+                }
+
+    for v in nonoptimVars[model]:
+        metadata[model][i]["vars"].add %*{ "var": newJString v.name.strVal, 
+                                    "init": newJString v.asgn.strVal,
+                                    "shape": parseJson(repr v.shape),
+                                    "optim_var": true
+                                }
+
+        prgm.variable(model, 
+                    scope,
+                    newCall("empty", scope, 
+                                    prgm.nconst(model, scope, v.shape, ident "oint32"), 
+                                    v.dtype),
+                    v.shape,
+                    ident name & "_" & $v.name,
+                    ident name & "_" & $v.asgn,
+                    optim=true)
