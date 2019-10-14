f291a7c978e4ef2a4cdd7288e472cfd012a7efeb
Martin Reichholf <martin@Martins-MBP.home>
2019-10-14 20:52:00 +0200

    refactor

--- tensorflow/ops/newop/register_context.nim
+++ tensorflow/ops/newop/register_context.nim
@@ -3,39 +3,41 @@ import ../../utils/utils
 {.hint[XDeclaredButNotUsed]:off.}
 
 type
-    OpKernelContext {.header: std_ops,
+    OpKernelContext* {.header: std_ops,
                       importcpp: "tensorflow::OpKernelContext".} = object
-    InferenceContext {.header: shape_inference,
+    InferenceContext* {.header: shape_inference,
                        importcpp: "tensorflow::shape_inference::InferenceContext".} = object
+    IteratorContext* {.header: dataset,
+                      importcpp: "tensorflow::IteratorContext".} = object
 
-proc set_output(ctx: ptr InferenceContext, i: int, status: Status) {.header: std_ops,
+
+proc set_output*(ctx: ptr InferenceContext, i: int, status: Status) {.header: std_ops,
                                                                      importcpp: "#->set_output(#, #)".}
 
-proc input(ctx: ptr InferenceContext, i: int): Status {.header: std_ops,
+proc input*(ctx: ptr InferenceContext, i: int): Status {.header: std_ops,
                                                         importcpp: "#->input(#)".}
 
-proc input(ctx: ptr OpKernelContext, i: int): Tensor {.header: op_kernel,
-                                                       header: memory,
-                                                       importcpp: "std::make_shared<tensorflow::Tensor>(std::move(#->input(#)))".}
+proc input*(ctx: ptr OpKernelContext, i: int, T: type): Tensor[T] {.
+    header: op_kernel,
+    header: memory,
+    importcpp: "(tensorflow::Tensor *)&#->input(#)".}
 
-proc output(ctx: ptr OpKernelContext, i: int): Tensor {.header: op_kernel,
-                                                        header: memory,
-                                                        importcpp: "std::make_shared<tensorflow::Tensor>(std::move(#->output(#)))".}
+proc output*(ctx: ptr OpKernelContext, i: int, T: type): Tensor[T] {.
+    header: op_kernel,
+    header: memory,
+    importcpp: "(tensorflow::Tensor *)&#->output(#)".}
 
-proc set_output(ctx: ptr OpKernelContext, i: int, ten: Tensor) {.header: std_ops,
-                                                                 importcpp: "#->set_output(#, *#)".}
+proc set_output*[T](ctx: ptr OpKernelContext, i: int, ten: ref Tensor[T]) {.
+    header: std_ops,
+    importcpp: "#->set_output(#, **#)".}
 
-proc allocate_output(ctx: ptr OpKernelContext, i: int, shape: TensorShape, output: Tensor): Status {.header: op_kernel,
-                                                                                                     importcpp: "[&]() { auto _ctx = #; auto _i = #; auto _shape = #; tensorflow::Tensor* _tensorref = (tensorflow::Tensor*)(#.get()); return _ctx->allocate_output(_i, _shape, &_tensorref); } ()".}
+proc allocate_output*[T](ctx: ptr OpKernelContext, i: int, shape: TensorShape, output: ref Tensor[T]): Status {.
+    header: op_kernel,
+    importcpp: "#->allocate_output(#, #, #)".}
 
-proc OP_REQUIRES_OK(ctx: ptr OpKernelContext, status: Status) {.header: op_kernel,
+proc OP_REQUIRES_OK*(ctx: ptr OpKernelContext, status: Status) {.header: op_kernel,
                                                                 importcpp: "OP_REQUIRES_OK(#, #)".}
-
-export OpKernelContext,
-       InferenceContext,
-       set_output,
-       input,
-       output,
-       set_output,
-       allocate_output,
-       OP_REQUIRES_OK
+                                                            
+proc allocator*(ctx: ptr IteratorContext): ptr Allocator {.
+    header: dataset, 
+    importcpp:"#->allocator({})".}
