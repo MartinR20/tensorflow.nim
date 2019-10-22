56bea056b5ec0896985975847c957ac0fe5766c7
Martin Reichholf <martin@Martins-MBP.home>
2019-10-22 20:01:33 +0200

    made conv2d and transpose_conv2d seperate

--- tests/layers/conv2d.nim
+++ tests/layers/conv2d.nim
@@ -13,7 +13,26 @@ test "conv2d":
                        [[2], [2], [2]],
                        [[3], [3], [3]]]], ofloat)
     
-    model m1, scope, sess:
+    model m1x0, scope, sess:
+        input ten, [1,3,3,1], ofloat
+        conv2d 3, [2, 2], [2, 2]
+        vars m
+        vars v
+        optim applyAdam vars, m, v, 0, 0, 1e-4, 0.9, 0.99, 10e-8
+        init
+        run 
+
+    delete ten
+
+test "transpose_conv2d": 
+    let scope = newRootScope()
+    let sess = scope.newSession()
+
+    let ten = tensor([[[[1], [1], [1]],
+                       [[2], [2], [2]],
+                       [[3], [3], [3]]]], ofloat)
+    
+    model m1x1, scope, sess:
         input ten, [1,3,3,1], ofloat
         conv2d 3, [2, 2], [2, 2]
         conv2d_transpose 3, [2, 2], [2, 2]
