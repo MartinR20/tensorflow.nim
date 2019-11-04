f3e3cc556449a01517135d1d355b677aa218fa13
Martin Reichholf <martin@Martins-MBP.home>
2019-11-04 11:45:58 +0100

    added inputs

---
+++ src/tensorflow/layersv3/inputs.nim
@@ -0,0 +1,18 @@
+from nodes import
+    Ctx, nshape, dtype, input, scope 
+
+from ../ops import 
+    placeholder
+
+export placeholder
+
+template input*(ctx: Ctx,
+                    T: type,
+                    inshape: seq[int]): untyped = 
+
+    ctx.nshape(inshape)
+    ctx.dtype(T)
+
+    let placeh = placeholder(ctx.scope, T, inshape.shape).output
+    ctx.feed("x", placeh)
+    ctx.input placeh
