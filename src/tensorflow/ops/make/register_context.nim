23e111273a32e90e603fcaec8257b17efefa20fa
Martin Reichholf <martin@Martins-MBP.home>
2019-11-04 11:42:40 +0100

    started adding more functionallity

--- src/tensorflow/ops/make/register_context.nim
+++ src/tensorflow/ops/make/register_context.nim
@@ -2,12 +2,14 @@ import ../../core, ../../utils
 {.hint[XDeclaredButNotUsed]:off.}
 
 type
-    OpKernelContext* {.header: std_ops,
+    OpKernelContext* {.header: op_kernel,
                       importcpp: "tensorflow::OpKernelContext".} = object
     InferenceContext* {.header: shape_inference,
                        importcpp: "tensorflow::shape_inference::InferenceContext".} = object
     IteratorContext* {.header: dataset,
                       importcpp: "tensorflow::IteratorContext".} = object
+    Mutex* {.header: op_kernel,
+             importcpp: "tensorflow::mutex*".} = object
 
 
 proc set_output*(ctx: ptr InferenceContext, i: int, status: Status) {.header: std_ops,
@@ -16,19 +18,57 @@ proc set_output*(ctx: ptr InferenceContext, i: int, status: Status) {.header: st
 proc input*(ctx: ptr InferenceContext, i: int): Status {.header: std_ops,
                                                         importcpp: "#->input(#)".}
 
-proc input*(ctx: ptr OpKernelContext, i: int, T: type): Tensor[T] {.
+proc input_dtype*(ctx: ptr OpKernelContext, i: int): DType {.
     header: op_kernel,
     header: memory,
-    importcpp: "(tensorflow::Tensor *)&#->input(#)".}
+    importcpp: "#->input_dtype(#)".}
+
+proc iinput[T](ctx: ptr OpKernelContext, name: cppstring, tensor: ref Tensor[T]): Status {.
+    header: op_kernel,
+    header: memory,
+    importcpp: "#->input(#, (const tensorflow::Tensor**)#)".}
+
+proc input*[T](ctx: ptr OpKernelContext, name: string, tensor: ref Tensor[T]): Status = 
+    result = ctx.iinput(name.newCPPString, tensor)
+
+proc iinput_ref_mutex(ctx: ptr OpKernelContext, name: cppstring, mutex: ref Mutex): Status {.
+    header: op_kernel,
+    header: memory,
+    importcpp: "#->input_ref_mutex(#, #)".}
+
+proc input_ref_mutex*(ctx: ptr OpKernelContext, name: string, mutex: ref Mutex): Status =
+    result = ctx.iinput_ref_mutex(name.newCPPString, mutex)
+
+proc lock*(mutex: Mutex) {.
+    header: op_kernel,
+    header: memory,
+    importcpp: "tensorflow::mutex_lock lock(*#)".}
+
+proc imutable_input[T](ctx: ptr OpKernelContext, name: cppstring, tensor: Tensor[T], lock_held: bool): Status {.
+    header: op_kernel,
+    header: memory,
+    importcpp: "#->mutable_input(#, #, #)".}
+
+proc mutable_input*[T](ctx: ptr OpKernelContext, name: string, tensor: Tensor[T], lock_held: bool): Status =
+    result = ctx.imutable_input(newCPPString(name), tensor, lock_held)
 
 proc output*(ctx: ptr OpKernelContext, i: int, T: type): Tensor[T] {.
     header: op_kernel,
     header: memory,
     importcpp: "(tensorflow::Tensor *)&#->output(#)".}
 
+proc set_status*(ctx: ptr OpKernelContext, s: Status) {.
+    header: op_kernel,
+    header: memory,
+    importcpp: "#->SetStatus(#)".}
+
 proc set_output*[T](ctx: ptr OpKernelContext, i: int, ten: ref Tensor[T]) {.
     header: std_ops,
     importcpp: "#->set_output(#, **#)".}
+    
+proc set_output*[T](ctx: ptr OpKernelContext, i: int, ten: Tensor[T]) {.
+    header: std_ops,
+    importcpp: "#->set_output(#, *#)".}
 
 proc allocate_output*[T](ctx: ptr OpKernelContext, i: int, shape: TensorShape, output: ref Tensor[T]): Status {.
     header: op_kernel,
