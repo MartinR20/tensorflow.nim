11e533355bd4ef42475cea7469e0bad894beb61d
Martin Reichholf <martin@Martins-MBP.home>
2019-11-04 11:45:50 +0100

    added flatten

---
+++ src/tensorflow/layersv3/flattens.nim
@@ -0,0 +1,22 @@
+from nodes import 
+    Ctx, nshape, input, scope
+
+include ../with
+
+from sequtils import 
+    toSeq, foldl
+
+export toSeq
+
+from ../ops import 
+    reshape
+
+export reshape
+
+template flatten*(ctx: static Ctx): untyped =
+    const old_shape = ctx.nshape
+    const new_shape = [old_shape[0], old_shape[1..^1].foldl(a * b)]
+    ctx.nshape new_shape.toSeq
+
+    with ctx.scope:
+        ctx.input reshape(ctx.input, new_shape.oint32).output
