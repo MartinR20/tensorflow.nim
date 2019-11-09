6ee2ad2d358b2814a96aa68f46b3c5f58258fe36
Martin Reichholf <martin@Martins-MBP.home>
2019-11-09 21:21:45 +0100

    reduced mean and added debug output

--- src/tensorflow/layersv3/losses.nim
+++ src/tensorflow/layersv3/losses.nim
@@ -12,9 +12,13 @@ template mse*(ctx: static Ctx): untyped =
     let y = placeholder(ctx.scope, ctx.dtype, shape ctx.nshape).output
     ctx.feed("y", y)
 
+    when defined debug:
+        ctx.output ctx.input
+
     with ctx.scope:
         let diff = y - ctx.input
         ctx.input mean(diff * diff, 0.oint32).output
+        ctx.input mean(ctx.input, 0.oint32).output
 
     when defined debug:
         ctx.loss ctx.input
