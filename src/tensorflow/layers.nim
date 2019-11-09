526771a4d0607914c5f79372cb98b9dafdb42863
Martin Reichholf <martin@Martins-MBP.home>
2019-11-09 21:31:07 +0100

    made import

---
+++ src/tensorflow/layers.nim
@@ -0,0 +1,8 @@
+import
+    layersv3/activs, layersv3/conv2ds, layersv3/denses,
+    layersv3/inputs, layersv3/layers, layersv3/losses, 
+    layersv3/optims, layersv3/reshapes, layersv3/rnn
+
+export 
+    activs, conv2ds, denses, inputs, layers, losses, 
+    optims, reshapes, rnn
--- src/tensorflow/layersv2.nim
+++
@@ -1,5 +0,0 @@
-import
-    layersv2/layersv2
-
-export 
-    layersv2
